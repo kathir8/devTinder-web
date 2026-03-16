73cf89e04bd93e0fe1158c3db864a138a6196140
Kathiravan <sukn99@gmail.com>
2026-03-16 09:19:37 +0530

    profile view update

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,34 @@
-import { Outlet } from 'react-router-dom'
-import NavBar from './NavBar'
-import Footer from './Footer'
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { BASE_URL } from "../utils/constants";
+import { addUser } from '../utils/userSlice';
+import Footer from './Footer';
+import NavBar from './NavBar';
+import axios from 'axios';
 
 const Body = () => {
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const userData = useSelector((state) => state.user);
+    const fetchUser = async () => {
+        if(userData) return;
+        try {
+            const res = await axios.get(BASE_URL + '/profile/view', { withCredentials: true });
+            dispatch(addUser(res.data))
+        } catch (err) {
+            if(err.status === 401){
+                navigate("/login");
+            }
+            console.error(err);
+        }
+    }
+
+    useEffect(() => {
+        fetchUser();
+    }, []);
+
     return (
         <>
             <NavBar />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,25 @@
 import { useSelector } from "react-redux";
+import { BASE_URL } from "../utils/constants";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
 
     const user = useSelector((state) => state.user);
 
+    const logOut = async () => {
+        try {
+            await axios.get(`${BASE_URL}/logout`, { withCredentials: true });
+            window.location.reload();
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
     return (
         <div className="navbar bg-base-300">
             <div className="flex-1">
-                <a className="btn btn-ghost text-xl">👩‍💻 DevTinder</a>
+                <Link to="/" className="btn btn-ghost text-xl">👩‍💻 DevTinder</Link>
             </div>
             <div className="flex gap-2">
                 {user && (<>
@@ -24,13 +36,13 @@ const NavBar = () => {
                             tabIndex="-1"
                             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
                             <li>
-                                <a className="justify-between">
+                                <Link to="/profile" className="justify-between">
                                     Profile
                                     <span className="badge">New</span>
-                                </a>
+                                </Link>
                             </li>
-                            <li><a>Settings</a></li>
-                            <li><a>Logout</a></li>
+                            <li><Link to="/settings">Settings</Link></li>
+                            <li onClick={logOut}><a>Logout</a></li>
                         </ul>
                     </div>
                 </>)}
