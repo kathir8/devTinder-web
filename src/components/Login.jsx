d1573bfbb025b5f5524ff7d42d5e85a9fe062427
kathiravan <kathiravans@boodskap.io>
2026-03-16 06:02:20 +0530

    redux toolkit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,22 @@
 import axios from "axios";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
 
   const [emailId, setEmailId] = useState('akshay@gmail.com');
   const [password, setPassword] = useState('Akshay@123');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleLogin = async () => {
     try {
-      const res = await axios.post('http://localhost:3000/login', { emailId, password }, { withCredentials: true });
-      console.log(res.data);
+      const res = await axios.post(`${BASE_URL}/login`, { emailId, password }, { withCredentials: true });
+      dispatch(addUser(res.data));
+      return navigate('/');
     } catch (err) {
       console.log(err);
     }
