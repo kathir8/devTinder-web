60048a72c300673f6528a994f505888fea1f1f67
kathiravan <kathiravans@boodskap.io>
2026-03-20 04:21:06 +0530

    integrate: request send api

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,6 +1,22 @@
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { removeUserFromFeed } from "../utils/feedSlice";
 
 const UserCard = ({ user, fromProfile }) => {
-    const { firstName, lastName, about, profileImage, age, gender } = user;
+    const { firstName, lastName, about, profileImage, age, gender, _id } = user;
+
+    const dispatch = useDispatch();
+
+    const sendRequest = async (status, id) => {
+        try {
+            const res = await axios.post(`${BASE_URL}/request/send/${status}/${id}`, {}, { withCredentials: true });
+            dispatch(removeUserFromFeed(id));
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
     return (
         <div className="card bg-base-300 w-96 shadow-sm">
             <figure>
@@ -13,8 +29,8 @@ const UserCard = ({ user, fromProfile }) => {
                 {age && gender && <p>{age}, {gender}</p>}
                 <p>{about || "A card component has a figure, a body part, and inside body there are title and actions parts"}</p>
                 {!fromProfile && <div className="card-actions justify-end">
-                    <div className="btn btn-primary">Ignore</div>
-                    <div className="btn btn-secondary">Interested</div>
+                    <div className="btn btn-primary" onClick={() => sendRequest('ignored', _id)}>Ignore</div>
+                    <div className="btn btn-secondary" onClick={() => sendRequest('interested', _id)}>Interested</div>
                 </div>}
             </div>
         </div>
